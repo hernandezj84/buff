bd548f12218324e1b2228adc3d5424d9e6eeecc8
Jonathan Hernandez <hernandezj84@gmail.com>
2019-10-17 18:56:56 -0500

    #M Add JSONRENDERER

--- buff/settings.py
+++ buff/settings.py
@@ -79,6 +79,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny'
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+
 }
 
 
