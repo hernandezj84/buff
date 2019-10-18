9cd4ad9147ae558e4c3df57d2f30c50c9b054b03
Jonathan Hernandez <hernandezj84@gmail.com>
2019-10-18 09:40:09 -0500

    #M Add corsheaders packages

--- buff/settings.py
+++ buff/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [u'developer98code.pythonanywhere.com']
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -39,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
