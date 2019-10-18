ad226ff25c9bf8cd633c56cd43b5318041046f17
Jonathan Hernandez <hernandezj84@gmail.com>
2019-10-18 09:48:25 -0500

    #A Add permission classes to test

--- api/views.py
+++ api/views.py
@@ -1,12 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from api.models import Workout
 
 
 @api_view(['GET'])
+@permission_classes((AllowAny,))
 def test(request):
     return Response({"data": "hello world"})
 
