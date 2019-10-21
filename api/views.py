1ddea3dc111380d380a84742b3d37f35046e76f2
Jonathan Hernandez <hernandezj84@gmail.com>
2019-10-21 16:31:39 -0500

    #A another change

--- api/views.py
+++ api/views.py
@@ -26,7 +26,7 @@ def test_jwt(request):
             post_data, settings.SECRET_KEY, algorithms=['HS256'])
         data["data"] = jwt_decoded
     except Exception as e:
-        data["error"] = str(e) + str(request.data)
+        data["error"] = str(e)
 
     return Response(data)
 
