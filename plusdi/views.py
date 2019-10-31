26c606f7b8ca30d6aa48a26948c8fe458cbc702a
Jonathan Hernandez <hernandezj84@gmail.com>
2019-10-31 15:07:18 -0500

    Add test url

--- plusdi/views.py
+++ plusdi/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from api.models import Workout
+from django.contrib.auth import authenticate
+from jose import jwt
+from django.conf import settings
+import json
 
 # Create your views here.
+
+
+@api_view(['POST'])
+@permission_classes((AllowAny,))
+def test(request):
+    data = {"message": "test completed"}
+    try:
+        data = {""}
+    except Exception as e:
+        data["error"] = str(e)
+
+    return Response(data)
